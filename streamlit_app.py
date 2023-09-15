652073e351a2a3303d0204943c5f5c7d50d9c416
Amine <abbamine@gmail.com>
2023-09-15 12:28:30 +0100

    Update streamlit_app.py
    add pandas dataframe & display data from csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,17 @@
 import streamlit as st
+import pandas as ps
+
 st.title( "Snowflake - ❄️ -" )
 st.header( "Data Application Builders" )
 st.text( "Workshop - 15/09/2023" )
 st.text( "*************************************" )
 
-st.header("Breakfast Menu")
-st.text("🥣Omega 3 & Blueberry Oatmeal")
+st.header('Breakfast Menu')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
 st.text('🥗Kale, Spinach & Rocket Smoothie')
 st.text('🐔Hard-Boiled Free-Range Egg')
 st.text('🥑🍞Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = ps.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
