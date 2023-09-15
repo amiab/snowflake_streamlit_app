99c8ac8c25af767bfd7f9854c7e60286f7e44f52
Amine <abbamine@gmail.com>
2023-09-15 12:14:43 +0100

    Update streamlit_app.py
    snowflake workshop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 st.title( "Snowflake - ❄️ -" )
 st.header( "Data Application Builders" )
-st.text( "Workshop..." )
+st.text( "Workshop - 15/09/2023" )
+st.text( "*************************************" )
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
